2c2192aff94f977b9900a6fe7d48c885f8c608be
jbr284 <jbrosa2009@gmail.com>
2026-04-16 21:35:51 -0300

    Update authConfig.ts

--- src/authConfig.ts
+++ src/authConfig.ts
@@ -1,10 +1,12 @@
 export const msalConfig = {
     auth: {
-        // Lembre-se de colar os códigos que você gerou lá no portal do Azure aqui:
-        clientId: "COLE_AQUI_SEU_CLIENT_ID_DO_AZURE",
-        authority: "https://login.microsoftonline.com/COLE_AQUI_SEU_TENANT_ID_DO_AZURE",
+        // ID do aplicativo (cliente) que você enviou
+        clientId: "1cd98e38-5f34-428c-9165-4a70cf27b853",
         
-        // O seu link exato do GitHub
+        // Link com o ID do diretório (locatário) que você enviou
+        authority: "https://login.microsoftonline.com/63123b37-50ce-444c-80fa-fe99d1bf9f46",
+        
+        // O link oficial do seu PWA no GitHub Pages
         redirectUri: "https://jbr284.github.io/PESQUISA-DE-CORTES-LASER_SERRA/",
     },
     cache: {
